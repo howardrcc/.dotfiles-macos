ce3999d607e4c152c94888d780daa5da11a33cc3
Howard Ching Chung <howard@Howards-MacBook-Pro.local>
2024-12-23 21:29:43 +0100

    first commit

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # Welcome to my dotfiles for macOS
 
+![image](images/dotfiles-01.jpg)
+
+## Features
+
+- window manager using aerospace
+- (way)bar using sketchybar
+- neovim
+- (optional) lazyvim
+- manage .rc files using rcm
+
+
 
 ## clone this repo and run install.sh; or
 
@@ -7,7 +18,8 @@ or:
 
 ## install brew
 
-## install rc files using rcm
+## use rcm to install/symlink rc-files
+
 
 ## install software/apps
 
@@ -18,3 +30,9 @@ or:
 ## allow (homebrew) via terminal
 
 xattr -d com.apple.quarantine /Applications/Alacritty.app
+
+### thanks to
+
+- https://github.com/JaKooLit/Wallpaper-Bank
+- https://github.com/FelixKratz/SketchyBar
+- bin101 ???
--- brew_apps.sh
+++ brew_apps.sh
@@ -5,11 +5,28 @@ brew install --HEAD utf8proc
 brew install luarocks
 brew install lua
 brew install alacritty
+brew install --cask kitty
 brew install font-meslo-lg-nerd-font
 brew install powerlevel10k
 brew install zsh-autosuggestions
 brew install zsh-syntax-highlighting
-brew install zoxide
-brew install eza
+brew install zoxide #smarter cd
+brew install eza #smarter ls
 
 #some brews in sketchybar/helpers
+# Packages
+brew install switchaudio-osx
+brew install nowplaying-cli
+
+brew tap FelixKratz/formulae
+brew install sketchybar
+
+# Fonts
+brew install --cask sf-symbols
+brew install --cask homebrew/cask-fonts/font-sf-mono
+brew install --cask homebrew/cask-fonts/font-sf-pro
+brew install --cask visual-studio-code
+curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v2.0.5/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
+
+# SbarLua
+(git clone https://github.com/FelixKratz/SbarLua.git /tmp/SbarLua && cd /tmp/SbarLua/ && make install && rm -rf /tmp/SbarLua/)
